dac1c757e3af6a4bc7afba86bfc7489fdae5aa57
Fer De La Rosa <fer96.dlrs@gmail.com>
2022-08-09 21:31:50 -0500

    Add `continuations` examples

--- ConcurrencyExamples.playground/Contents.swift
+++ ConcurrencyExamples.playground/Contents.swift
@@ -74,3 +74,51 @@ asyncExample("Handle async let properties") {
 
   await loadData()
 }
+
+// MARK: - Continuations
+asyncExample("Continuations with throwing errors") {
+  enum FetchError: Error {
+    case noMessages
+  }
+
+  struct Message: Decodable, Identifiable {
+    let id: Int
+    let from: String
+    let message: String
+  }
+
+  func fetchMessages(completion: @escaping ([Message]) -> Void) {
+    let url = URL(string: "https://hws.dev/user-messages.json")!
+    URLSession.shared.dataTask(with: url) { data, response, error
+      in
+      if let data = data {
+        if let messages = try?
+            JSONDecoder().decode([Message].self, from: data) {
+          completion(messages)
+          return
+        }
+      }
+      completion([])
+    }.resume()
+  }
+
+  func fetchMessages() async -> [Message] {
+    do {
+      return try await withCheckedThrowingContinuation
+      { continuation in
+        fetchMessages { messages in
+          if messages.isEmpty {
+            continuation.resume(throwing: FetchError.noMessages)
+          } else {
+            continuation.resume(returning: messages)
+          }
+        }
+      }
+    } catch {
+      return [Message(id: 1, from: "Tom", message: "Welcome to MySpace! I'm your new friend.")]
+    }
+  }
+
+  let messages = await fetchMessages()
+  print("Downloaded \(messages.count) messages.")
+}
